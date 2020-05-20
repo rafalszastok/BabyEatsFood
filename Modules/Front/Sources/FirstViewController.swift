de865f9c36d7a30017d279256554465f3fa15ae1
Rafal Szastok <Rafal.Szastok@dazn.com>
2020-05-20 20:46:19 +0200

    Setup Carthage, Swiftformat and Xcodegen

---
+++ Modules/Front/Sources/FirstViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.swift
+//  BabyEatsFood
+//
+//  Created by Rafal Szastok on 19/05/2020.
+//  Copyright © 2020 Rafal Szastok. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Modules/Front/Sources/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BabyEatsFood
+//
+//  Created by Rafal Szastok on 19/05/2020.
+//  Copyright © 2020 Rafal Szastok. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
